bf91dd43e846483d80237e67085f40013539bccd
Saeki Hironori <saeki.hironori@itfllc.co.jp>
2023-05-01 13:09:32 +0900

    clone vite folder

---
+++ vite-react-todo-app/src/components/LoginButton.tsx
@@ -0,0 +1,21 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@chakra-ui/button";
+import React from "react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <Button
+      colorScheme="blue"
+      onClick={() => {
+        loginWithRedirect();
+        console.log("動いた！");
+      }}
+    >
+      Login
+    </Button>
+  );
+};
+
+export default LoginButton;
